1ff07b965252786802c8c20dea2faf3daaa20d07
denver daniels <denver@projectcodex.co>
2016-08-02 08:52:10 +0200

    updated app

--- app.js
+++ app.js
@@ -2,68 +2,52 @@ var http = require('http');
 var express = require('express'),
     app = module.exports.app = express(),
     io = require('socket.io'),
-    fs = require('fs'),
     five = require('johnny-five'),
-    board, ping, led1, led2, led3;
-
-
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-server.listen(8060);
-
+    board, ping;
 
 board = new five.Board();
-
 app.use(express.static('public'));
 
 board.on("ready", function() {
 
-    ping   = new five.Ping(7);
-    red    = new five.Led(11);
-    orange = new five.Led(12);
-    green  = new five.Led(13);
+    ping = new five.Ping(7);
 
-
-    console.log("Board is Ready")
+    console.log("Board is Ready");
 
     ping.on('change', function(value) {
-        console.log("DIstance:" + this.in)
+        console.log("Distance: " + this.in);
 
-        var distance = "object" + " " + this.in + " " + "inches away";
+        var distance = "Object " + this.in + " inches away";
         io.emit('motion', {
             distance: distance
-        })
-        switch (this.in) {
-            case this.in < 4:
-                var redlight = this.in < 4;
-                red.on();
-                green.off();
-                orange.off();
-                io.emit('color-red', {
-                    redlight: redlight
-                })
-                console.log("light is red");
-                break;
-            case this.in > 4 && this.in < 11:
-                var orangeLight = this.in == 11;
-                orange.on();
-                red.off();
-                green.off();
-                io.emit('color-orange', {
-                    orangeLight: orangeLight
-                })
-                console.log("light is orange");
-                break;
-            case this.in >= 11.33:
-                var greenLight = this.in >= 11.33;
-                orange.off();
-                red.off();
-                green.on();
-                io.emit('color-green', {
-                    greenLight: greenLight
-                })
-                console.log("light is green");
-                break;
+        });
+
+        if (this.in < 4) {
+            var redlight = this.in < 4;
+            io.emit('color-red', {
+                redlight: redlight
+            });
+            console.log("light is red");
+
+        } else if (this.in > 4 && this.in < 11) {
+            var orangeLight = this.in == 11;
+            io.emit('color-orange', {
+                orangeLight: orangeLight
+            });
+            console.log("light is orange");
+
+        } else if (this.in >= 11.33) {
+            var greenLight = this.in >= 11.33;
+            io.emit('color-green', {
+                greenLight: greenLight
+            });
+            console.log("light is green");
         };
-    })
+    });
 });
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.port || 8060;
+server.listen(port);
+console.log('App running on port ', port);
